594aa724e93400f1b9abd8c619ef585f7297d267
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-06-12 16:12:07 +0200

    Fixing metadata

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -131,7 +131,8 @@ def training_trainer(modelName, train_dataset, dev_dataset, eval_dataset, num_tr
                                            model_path=best_model_path,
                                            tokenizer_name=tokenizer.name_or_path,
                                            verbose=False,
-                                           lang=eval_data_lang)
+                                           lang=eval_data_lang,
+                                           add_lang_metadata=add_lang_metadata)
     
     
     

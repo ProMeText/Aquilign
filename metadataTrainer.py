3c7a1952334d0c4f74c5da85fd51568d9a86f01c
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-12-07 18:54:27 +0100

    Trying to mitigate

--- metadataTrainer.py
+++ metadataTrainer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
-from transformers import BertTokenizer, Trainer, TrainingArguments, AutoModelForTokenClassification, set_seed
+from transformers import BertTokenizer, Trainer, TrainingArguments, AutoModelForTokenClassification, set_seed, TrainerCallback
 from accelerate import Accelerator, DataLoaderConfiguration
 import aquilign.preproc.tok_trainer_functions as trainer_functions
 import aquilign.preproc.eval as evaluation

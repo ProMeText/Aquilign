87ed7185b794988115fc2bf8c76799c792b38e57
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-08-11 19:24:15 +0200

    Change main

--- aquilign/segmenter/segmenters.py
+++ aquilign/segmenter/segmenters.py
@@ -6,10 +6,13 @@ import aquilign.segmenter.trainer as trainer
 
 
 if __name__ == '__main__':
-	device = "cpu"
-	test_path = "/home/mgl/Bureau/Travail/projets/alignement/alignement_multilingue/multilingual-segmentation-dataset/data/Multilingual_Aegidius/segmented/split/multilingual/test.json"
-	train_path = "/home/mgl/Bureau/Travail/projets/alignement/alignement_multilingue/multilingual-segmentation-dataset/data/Multilingual_Aegidius/segmented/split/multilingual/train.json"
-	output_dir = "/home/mgl/Documents/lstm/"
+	device = sys.argv[4]
+	train_path = sys.argv[1]
+	test_path = sys.argv[2]
+	output_dir = sys.argv[3]
+	# test_path = "/home/mgl/Bureau/Travail/projets/alignement/alignement_multilingue/multilingual-segmentation-dataset/data/Multilingual_Aegidius/segmented/split/multilingual/test.json"
+	# train_path = "/home/mgl/Bureau/Travail/projets/alignement/alignement_multilingue/multilingual-segmentation-dataset/data/Multilingual_Aegidius/segmented/split/multilingual/train.json"
+	# output_dir = "/home/mgl/Documents/lstm/"
 	trainer = trainer.Trainer(architecture="lstm",
 							  epochs=1,
 							  batch_size=32,
